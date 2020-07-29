5a13f386aa03de7ff2cbb3a01278a4993bd0af27
Nelson Liu <nfliu@nelsonliu.me>
2020-07-28 22:34:35 -0700

    Add support for parsing globecoordinate data type

---
+++ tests/globecoordinate_test.py
@@ -0,0 +1,23 @@
+from pytest import fixture
+
+from wikidata.client import Client
+from wikidata.entity import Entity
+from wikidata.globecoordinate import GlobeCoordinate
+
+
+@fixture
+def fx_globecoordinate() -> GlobeCoordinate:
+    client = Client()
+    return GlobeCoordinate(**{
+        "latitude": 70.1525,
+        "longitude": 70.1525,
+        "precision": 0.0002777777777777778,
+        "globe": client.get("Q111")})
+
+
+def test_globecoordinate_value(fx_globecoordinate: GlobeCoordinate):
+    assert fx_globecoordinate.latitude == 70.1525
+    assert fx_globecoordinate.longitude == 70.1525
+    assert fx_globecoordinate.precision == 0.0002777777777777778
+    assert isinstance(fx_globecoordinate.globe, Entity)
+    assert fx_globecoordinate.globe.id == "Q111"
